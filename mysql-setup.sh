85bb822d2e997bebff702b4ed64d626d0b47d36f
Chris Suran <christopher.suran@harness.io>
2023-11-23 13:47:26 -0500

    Remove root user create

--- mysql-setup.sh
+++ mysql-setup.sh
@@ -10,7 +10,6 @@ mysql < /work/Core3/MMOCoreORB/sql/swgemu.sql
 mysql -v <<-EOF
         SET @@SESSION.SQL_LOG_BIN=0;
         GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-        CREATE USER 'root'@'%' IDENTIFIED BY '${DBPASS}';
         GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
         CREATE USER '${DBUSER}'@'%' IDENTIFIED BY '${DBPASS}';
         GRANT ALL ON \`${DBNAME}\`.* TO '${DBUSER}'@'%' WITH GRANT OPTION;
